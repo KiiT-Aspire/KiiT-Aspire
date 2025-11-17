335aa37242ea2fd10ad813f8f3af707704ae02d8
MridulModi12 <modi.mridul0828@gmail.com>
2025-11-17 20:28:07 +0530

    Minor Change for Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -352,10 +352,6 @@ export default function Home() {
               </ul>
             </div>
           </div>
-
-          <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-500">
-            <p>&copy; 2025 EchoGrade. All rights reserved.</p>
-          </div>
         </div>
       </footer>
     </div>
